73fc59fece2693eedb25a16be6909b5abe8237f5
Eric Dattore <edattore@gmail.com>
2018-02-11 18:59:59 -0700

    Add basic CI support
    Uses Travis-CI and uses build stages so that (future) deployments are
    only deployed on success of unit tests on the frontend and backend.
    
    [#155073677]

--- titan-web-client/scripts/setup.sh
+++ titan-web-client/scripts/setup.sh
@@ -5,8 +5,8 @@ APP_PATH="${SCRIPT_PATH}/.."
 
 for d in src/*/; do
     if [[ ${d} = *"titan-"* ]]; then
-         MODULE_PATH=$(cut -d "/" -f 2 <<< "${d}")
-         echo "Linking module: ${MODULE_PATH}"
+        MODULE_PATH=$(cut -d "/" -f 2 <<< "${d}")
+        echo "Linking module: ${MODULE_PATH}"
 
         cd "${APP_PATH}/${d}"
         yarn link > /dev/null 2>&1
