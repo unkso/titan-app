f1e4166c8269f6e544683ccf02c3e7f087244b6e
MichaelJamesParsons <mjay.parsons@gmail.com>
2018-03-03 22:21:35 -0500

    Implemented text field

--- titan-web-client/src/titan-components/index.js
+++ titan-web-client/src/titan-components/index.js
@@ -1,3 +1,4 @@
 export {default as Button} from './button'
 export {default as FlatButton} from './flatButton'
 export {default as ContentBlock} from './block/ContentBlock'
+export {default as TextField} from './textField/TextField'
