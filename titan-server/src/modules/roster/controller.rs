c872a99165d3cd846cae3591de8be0668dc21bd7
Michael-James Parsons <mjay.parsons@gmail.com>
2018-05-05 23:57:44 -0400

    Implemented list members endpoint

--- titan-server/src/modules/roster/controller.rs
+++ titan-server/src/modules/roster/controller.rs
@@ -1,33 +1,18 @@
-use rocket_contrib::{Json, Value};
+use rocket_contrib::Json;
 use database::DbConn;
 use diesel;
 use diesel::prelude::*;
 use database::models::{RosterMember, NewRosterMember};
 use database::schema::roster_member;
-use serde_json;
 
 #[get("/roster")]
-pub fn get_roster() -> Json {
-    Json(json!([
-        RosterMember {
-            id: 1,
-            username: "MParsons".to_string(),
-            email: "mparsons@unkso.com".to_string(),
-            password: "NotMyPassword".to_string()
-        },
-        RosterMember {
-            id: 2,
-            username: "MParsons".to_string(),
-            email: "mparsons@unkso.com".to_string(),
-            password: "NotMyPassword".to_string()
-        },
-        RosterMember {
-            id: 3,
-            username: "MParsons".to_string(),
-            email: "mparsons@unkso.com".to_string(),
-            password: "NotMyPassword".to_string()
-        }
-    ]))
+pub fn get_roster(conn: DbConn) -> Json {
+    let results = roster_member::table
+        .order(roster_member::id.asc())
+        .load::<RosterMember>(&*conn)
+        .unwrap();
+
+    Json(json!(results))
 }
 
 #[post("/roster/member", data="<member>")]
@@ -36,7 +21,6 @@ pub fn create_member(conn: DbConn, member: Json<NewRosterMember>) -> Json<NewRos
         ..member.into_inner()
     };
 
-    // let r: NewRosterMember
     diesel::insert_into(roster_member::table)
         .values(m)
         .execute(&*conn)
