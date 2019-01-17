5c3c9622c52dfea5fb81d5b0a8bc823029a7dd9b
Eric Dattore <eric.dattore@gmail.com>
2019-01-16 20:47:40 -0700

    Remove need for rocket_cors in local dev
    Swapped server routes to /api and proxied requests that are unknown to
    the frontend app to the backend server to be serviced by Rocket

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Project Titan Monorepo
 The monolithic repository containing all the code for project titan.
 
+To start the app, run `bin/start`. As long as you have Rust and Yarn installed,
+the frontend and server should start up without issue.
--- titan-server/src/bin/main.rs
+++ titan-server/src/bin/main.rs
@@ -8,12 +8,10 @@ extern crate diesel;
 extern crate regex;
 extern crate bcrypt;
 extern crate frank_jwt;
-extern crate rocket_cors;
 
 use libtitan::routes;
 use libtitan::db;
 use libtitan::config;
-use libtitan::cors_fairing;
 use libtitan::accounts;
 use libtitan::organizations;
 
@@ -21,7 +19,6 @@ fn main() {
     rocket::ignite()
         .attach(db::TitanPrimary::fairing())
         .attach(db::UnksoMainForums::fairing())
-        .attach(cors_fairing::cors())
         .attach(AdHoc::on_attach("Config", |rocket| {
             let settings = config::AppConfig::from_env(&rocket.config());
             match settings {
@@ -29,9 +26,9 @@ fn main() {
                 Err(_) => Err(rocket),
             }
         }))
-        .mount("/auth/pulse", routes![routes::health_check])
-        .mount("/auth", accounts::get_auth_routes())
-        .mount("/users", accounts::get_user_routes())
-        .mount("/organizations", organizations::get_routes())
+        .mount("/api/auth/pulse", routes![routes::health_check])
+        .mount("/api/auth", accounts::get_auth_routes())
+        .mount("/api/users", accounts::get_user_routes())
+        .mount("/api/organizations", organizations::get_routes())
         .launch();
 }
