23958f7e0cb9b6dd305d06adaf8b23c2a7bcb8de
Eric Dattore <eric.dattore@gmail.com>
2019-01-30 19:11:28 -0700

    Refactor AppConfig fairing to impl

--- titan-server/src/bin/main.rs
+++ titan-server/src/bin/main.rs
@@ -7,18 +7,13 @@ use libtitan::db;
 use libtitan::config;
 use libtitan::accounts;
 use libtitan::organizations;
+use libtitan::config::AppConfig;
 
 fn main() {
     rocket::ignite()
         .attach(db::TitanPrimary::fairing())
         .attach(db::UnksoMainForums::fairing())
-        .attach(AdHoc::on_attach("Config", |rocket| {
-            let settings = config::AppConfig::from_env(&rocket.config());
-            match settings {
-                Ok(settings) => Ok(rocket.manage(settings)),
-                Err(_) => Err(rocket),
-            }
-        }))
+        .attach(AppConfig::fairing())
         .mount("/api/auth/pulse", routes![routes::health_check])
         .mount("/api/auth", accounts::get_auth_routes())
         .mount("/api/users", accounts::get_user_routes())
--- titan-server/src/config.rs
+++ titan-server/src/config.rs
@@ -1,15 +1,25 @@
-use rocket::config::ConfigError;
+use rocket::{config::ConfigError, fairing::{AdHoc, Fairing}};
 
 pub struct AppConfig {
     pub secret_key: String,
-    pub avatar_base_url: String
+    pub avatar_base_url: String,
 }
 
 impl AppConfig {
     pub fn from_env(config: &rocket::Config) -> Result<AppConfig, ConfigError> {
         Ok(AppConfig {
             avatar_base_url: config.get_str("avatar_base_url").unwrap().to_string(),
-            secret_key: config.get_str("auth_key").unwrap().to_string()
+            secret_key: config.get_str("auth_key").unwrap().to_string(),
+        })
+    }
+
+    pub fn fairing() -> impl Fairing {
+        AdHoc::on_attach("AppConfig", |rocket| {
+            let settings = Self::from_env(&rocket.config());
+            match settings {
+                Ok(settings) => Ok(rocket.manage(settings)),
+                Err(_) => Err(rocket),
+            }
         })
     }
 }
