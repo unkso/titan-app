c6faef2da1cc453c4f3f73ac0cf47e670c08f0a1
Michael-James Parsons <mjay.parsons@gmail.com>
2019-02-01 18:33:19 -0800

    Fixed login font styles

--- titan-web-client/src/layouts/empty-dark/index.js
+++ titan-web-client/src/layouts/empty-dark/index.js
@@ -1,6 +1,7 @@
-import styled, { withTheme } from 'styled-components';
+import styled, { createGlobalStyle, withTheme } from 'styled-components';
+import React from 'react';
 
-const EmptyDarkLayout = styled.div`
+const EmptyDarkLayoutStyle = styled.div`
   background-color: ${props => props.theme.palette.backgroundInversePrimary};
   position: absolute;
   top: 0;
@@ -8,5 +9,23 @@ const EmptyDarkLayout = styled.div`
   bottom: 0;
   left: 0;
 `;
+
+const GlobalStyles = createGlobalStyle`
+  html, body {
+    font-family: 'Roboto', sans-serif;
+    font-weight: 400;
+  }
+`;
+
+class EmptyDarkLayout extends React.Component {
+  render () {
+    return (
+      <React.Fragment>
+        <GlobalStyles />
+        <EmptyDarkLayoutStyle>{this.props.children}</EmptyDarkLayoutStyle>
+      </React.Fragment>
+    );
+  }
+}
 
 export default withTheme(EmptyDarkLayout);
