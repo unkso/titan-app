cc517d746e9df7eca0e9335ece1e9de2cabb7010
Eric Dattore <eric.dattore@gmail.com>
2019-02-20 17:14:57 -0700

    Add default set of event types to migration

--- titan-server/migrations/2019-02-09-024316_create_event_types_table/up.sql
+++ titan-server/migrations/2019-02-09-024316_create_event_types_table/up.sql
@@ -2,3 +2,14 @@ CREATE TABLE event_types (
   id INTEGER PRIMARY KEY AUTO_INCREMENT,
   name VARCHAR(255) UNIQUE
 );
+
+INSERT INTO event_types
+  (name)
+VALUES
+  ("Meeting"),
+  ("NCO Meeting"),
+  ("Practice"),
+  ("Scrim"),
+  ("Training"),
+  ("Exam"),
+  ("Other");
