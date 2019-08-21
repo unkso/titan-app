76a3a9becc1d6f8b591290352cfa8728f7e75906
Michael-James Parsons <mjay.parsons@gmail.com>
2019-08-20 23:56:29 -0700

    Roles view

--- titan-web-client/src/modules/organizations/organizationDetail/Roles.js
+++ titan-web-client/src/modules/organizations/organizationDetail/Roles.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import { ContentBlock } from 'titan/components/block/ContentBlock';
 import Card from '@material-ui/core/Card';
-import { CardContent, Typography } from '@material-ui/core';
-import { ListUsersRequest, useTitanApiClient } from 'titan/http/ApiClient';
+import { CardContent } from '@material-ui/core';
+import {
+  ListOrganizationRoles,
+  useTitanApiClient
+} from 'titan/http/ApiClient';
 import CardHeader from '@material-ui/core/CardHeader';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import DragIndicatorIcon from '@material-ui/icons/DragIndicator';
 
 export function Roles (props) {
   const fetchRoles = useTitanApiClient(
-    ListUsersRequest, { username: '', limit: 5 });
+    ListOrganizationRoles, { orgId: props.organization.id });
 
   return (
     <React.Fragment>
@@ -15,7 +23,18 @@ export function Roles (props) {
         <Card>
           <CardHeader title="Chain of Command" />
           <CardContent>
-            Goes here
+            {fetchRoles.data &&
+            <List>
+              {fetchRoles.data.filter(role => !!role.rank).forEach(role => (
+                <ListItem>
+                  <ListItemIcon>
+                    <DragIndicatorIcon />
+                  </ListItemIcon>
+                  <ListItemText>{role.role}</ListItemText>
+                </ListItem>
+              ))}
+            </List>
+            }
           </CardContent>
         </Card>
       </ContentBlock>
@@ -23,7 +42,18 @@ export function Roles (props) {
         <Card>
           <CardHeader title="Supporting Roles" />
           <CardContent>
-            Goes here
+            {fetchRoles.data &&
+            <List>
+              {fetchRoles.data.filter(role => !role.rank).forEach(role => (
+                <ListItem>
+                  <ListItemIcon>
+                    <DragIndicatorIcon />
+                  </ListItemIcon>
+                  <ListItemText>{role.role}</ListItemText>
+                </ListItem>
+              ))}
+            </List>
+            }
           </CardContent>
         </Card>
       </ContentBlock>
