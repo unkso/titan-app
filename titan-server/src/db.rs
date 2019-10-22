fc65ff86cfeeb268d5b5e2522c5c4761d3945fc1
Michael-James Parsons <mjay.parsons@gmail.com>
2019-10-21 23:02:52 -0700

    163 Api Responder

--- titan-server/src/db.rs
+++ titan-server/src/db.rs
@@ -1,5 +1,16 @@
 use rocket_contrib::databases::{database, diesel};
 
+pub enum TitanDatabaseError {
+    DieselError(diesel::result::Error),
+    UnexpectedResultCountError(String),
+}
+
+impl From<diesel::result::Error> for TitanDatabaseError {
+    fn from(err: diesel::result::Error) -> Self {
+        TitanDatabaseError::DieselError(err)
+    }
+}
+
 #[database("unkso_main_forums")]
 pub struct UnksoMainForums(diesel::MysqlConnection);
 
