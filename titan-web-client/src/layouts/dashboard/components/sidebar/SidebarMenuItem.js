be00cadc9d1e63f2c8d8196e1371faad9b284b79
Michael-James Parsons <mjay.parsons@gmail.com>
2019-11-17 11:59:15 -0800

    Add forums link in sidebar

--- titan-web-client/src/layouts/dashboard/components/sidebar/SidebarMenuItem.js
+++ titan-web-client/src/layouts/dashboard/components/sidebar/SidebarMenuItem.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import useTheme from '@material-ui/core/styles/useTheme';
 import styled from 'styled-components';
-import { withTheme } from '@material-ui/core/styles';
 import { Link } from 'react-router-dom';
 
 export const SidebarMenuItemIcon = styled.div`
@@ -20,64 +20,76 @@ export const SidebarMenuItemLabel = styled.div`
   flex: 1;
 `;
 
-export const SidebarMenuItemLink = styled(Link)`
-  padding: 12px 15px 12px 25px;
-  text-decoration: none;
-  display: flex;
-  flex-direction: row;
-  border-left: 2px solid ${props => props.style.borderColor};
-  
-  &:hover {
-    background-color: ${props => props.style.hoverBackground};
+export const SidebarMenuItemInternal = styled(Link)``;
+
+export const SidebarMenuItemLink = styled.div`
+  ${SidebarMenuItemInternal},
+  a {
+    border-left: 2px solid ${props => props.style.borderColor};
+    display: flex;
+    flex-direction: row;
+    padding: 12px 15px 12px 25px;
+    text-decoration: none;
+    
+    &:hover {
+      background-color: ${props => props.style.hoverBackground};
+    }
   }
 `;
 
-class SidebarMenuItemComponent extends React.Component {
-  render () {
-    let iconColor, textColor, borderColor;
-    if (this.props.isActive) {
-      iconColor = this.props.theme.palette.primary.main;
-      textColor = this.props.theme.palette.secondary.text;
-      borderColor = this.props.theme.palette.primary.main;
-    } else {
-      iconColor = this.props.theme.palette.inverseTextPrimary;
-      textColor = this.props.theme.palette.inverseTextPrimary;
-      borderColor = 'transparent';
-    }
-
-    return (
-      <SidebarMenuItemLink
-        to={this.props.url}
-        style={{
-          borderColor,
-          hoverBackground: this.props.theme.palette.backgroundInverseSecondary
-        }}
-      >
-        <SidebarMenuItemIcon color={iconColor}>
-          {this.props.leftIcon}
-        </SidebarMenuItemIcon>
-        <SidebarMenuItemLabel textColor={textColor}>
-          {this.props.label}
-        </SidebarMenuItemLabel>
-        <SidebarMenuItemIcon color={iconColor}>
-          {this.props.rightIcon}
-        </SidebarMenuItemIcon>
-      </SidebarMenuItemLink>
-    );
+export function SidebarMenuItem (props) {
+  const theme = useTheme();
+  let iconColor, textColor, borderColor;
+  if (props.isActive) {
+    iconColor = theme.palette.primary.main;
+    textColor = theme.palette.secondary.text;
+    borderColor = theme.palette.primary.main;
+  } else {
+    iconColor = theme.palette.inverseTextPrimary;
+    textColor = theme.palette.inverseTextPrimary;
+    borderColor = 'transparent';
   }
+
+  const template = (
+    <React.Fragment>
+      <SidebarMenuItemIcon color={iconColor}>
+        {props.leftIcon}
+      </SidebarMenuItemIcon>
+      <SidebarMenuItemLabel textColor={textColor}>
+        {props.label}
+      </SidebarMenuItemLabel>
+      <SidebarMenuItemIcon color={iconColor}>
+        {props.rightIcon}
+      </SidebarMenuItemIcon>
+    </React.Fragment>
+  );
+  const styles = {
+    borderColor,
+    hoverBackground: theme.palette.backgroundInverseSecondary
+  };
+
+  return (
+    <SidebarMenuItemLink style={styles}>
+      {props.isExternal ? (
+        <a href={props.url}>{template}</a>
+      ) : (
+        <SidebarMenuItemInternal
+          to={props.url}>{template}</SidebarMenuItemInternal>
+      )}
+    </SidebarMenuItemLink>
+  );
 }
 
-SidebarMenuItemComponent.propTypes = {
+SidebarMenuItem.propTypes = {
   url: PropTypes.string.isRequired,
   label: PropTypes.string.isRequired,
   leftIcon: PropTypes.object,
   rightIcon: PropTypes.object,
-  theme: PropTypes.object,
-  isActive: PropTypes.bool
+  isActive: PropTypes.bool,
+  isExternal: PropTypes.bool
 };
 
-SidebarMenuItemComponent.defaultProps = {
-  isActive: false
+SidebarMenuItem.defaultProps = {
+  isActive: false,
+  isExternal: false
 };
-
-export const SidebarMenuItem = withTheme(SidebarMenuItemComponent);
