664d2d07a5155dc9f0f1ccb7dbb185b831e32fd6
Michael-James Parsons <2666285+MichaelJamesParsons@users.noreply.github.com>
2020-02-13 13:51:20 -0800

    Remove extensions

--- titan-web-client/src/boot/index.ts
+++ titan-web-client/src/boot/index.ts
@@ -1,17 +1,13 @@
 import modules from '@titan/modules';
-import extensions from '@titan/extensions';
 import { setupStore } from '@titan/lib/redux/store';
 import titanConfig from '@titan/config';
-import { mountReducer, mountRoutes } from '@titan/boot/common';
+import { mountRoutes } from '@titan/boot/common';
 import {rootReducer} from "@titan/reducers";
 
 const routes = {};
 
 // Mount core modules
 modules.forEach(mountFeature);
-
-// Mount extensions
-extensions.forEach(mountFeature);
 
 function mountFeature (init) {
   const module = init();
